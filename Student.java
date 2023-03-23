656ee4b7416cf7ad5ecb00235d4e1ea102f642b4
mahammadnajafov <najafovmahammad@edu.bme.hu>
2023-03-23 22:17:55 +0100

    inital

--- Student.java
+++ Student.java
@@ -1,2 +1,14 @@
 public class Student {
+    //We need a constructor to ask their first name and the year
+
+    //we need to generate the ID
+
+    //we need to enroll them to the course
+
+    //we need to check the balance
+
+    //we need them to pay the tution
+
+
+    //and we need to show current status
 }
--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -1,2 +1,11 @@
 public class StudentDatabaseApp {
+    public static void main(String[] args) {
+        // we need to ask how many students will be there to add to the database
+
+
+
+        //
+        
+    }
 }
+8b
