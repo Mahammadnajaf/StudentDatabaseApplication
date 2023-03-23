9b04e5a8934390f49a944acc2a50147701eb5c32
mahammadnajafov <najafovmahammad@edu.bme.hu>
2023-03-23 23:35:34 +0100

    inital2

--- Student.java
+++ Student.java
@@ -4,7 +4,8 @@ public class Student {
     private String gradYear;
     private int StudentId;
     private String Courses;
-    aaaaaaa
+    private int tutionBalance;
+    private int costOfCourse=600;
 
 
     //We need a constructor to ask their first name and the year
